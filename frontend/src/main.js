3e2171d741076fda95fdd60394613daae59b9f28
SoWiEee <kawiisowieee@gmail.com>
2025-05-05 16:18:51 +0800

    add vuefity

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import vuetify from './vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
-const vuetify = createVuetify({
-    icons: {
-      defaultSet: 'mdi',
-      aliases,
-      sets: {
-        mdi,
-      },
-    },
-  })
 
 createApp(App).use(vuetify).mount('#app')
