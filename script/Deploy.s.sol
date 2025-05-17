464aa66974a16876b70e074fa0057aca528155cd
SoWiEee <kawiisowieee@gmail.com>
2025-05-17 15:52:27 +0800

    add deplot contract

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.13;
 
 import "lib/forge-std/src/Script.sol";
 import {EventLogger} from "../src/EventLogger.sol";
+import {MaliciousLogger} from "../src/MaliciousLogger.sol";
 
 contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
+        new MaliciousLogger();
         new EventLogger();
         vm.stopBroadcast();
     }
