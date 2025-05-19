d125c4dd7ac99251d9b6f03a037d939dc1aac815
SoWiEee <kawiisowieee@gmail.com>
2025-05-19 23:40:47 +0800

    add Pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'vuetify/styles'
+import { createPinia } from 'pinia'
 import vuetify from './vuetify'
+import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
-
-createApp(App).use(vuetify).use(router).mount('#app')
+const pinia = createPinia()
+createApp(App).use(vuetify).use(router).use(pinia).mount('#app')
