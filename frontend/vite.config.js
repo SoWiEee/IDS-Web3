2edc2245b11c0d9483375ea163c048025530d53c
SoWiEee <kawiisowieee@gmail.com>
2025-05-20 00:06:53 +0800

    update path alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import path from 'path'
 
 export default defineConfig({
+  root: __dirname,  // frontend = root
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),  // @ = src
     },
   },
   plugins: [
